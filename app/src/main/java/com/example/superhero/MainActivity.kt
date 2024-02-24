48b3870fc6fe58e95938a2973a294eb7d7eb15a2
josorian <josejaviersoriano54@gmail.com>
2024-02-25 00:00:36 +0100

    completo

--- app/src/main/java/com/example/superhero/MainActivity.kt
+++ app/src/main/java/com/example/superhero/MainActivity.kt
@@ -4,25 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.superhero.ui.theme.SuperHeroTheme
+import com.example.superhero.model.HeroesRepository
+import com.example.superhero.ui.theme.SuperheroesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SuperHeroTheme {
+            SuperheroesTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperheroesApp()
                 }
             }
         }
@@ -30,17 +35,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SuperheroesApp() {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar()
+        }
+    ) {
+        val heroes = HeroesRepository.heroes
+        HeroesList(heroes = heroes, contentPadding = it)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TopAppBar(modifier: Modifier = Modifier) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = stringResource(R.string.app_name),
+                style = MaterialTheme.typography.displayLarge,
+            )
+        },
         modifier = modifier
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    SuperHeroTheme {
-        Greeting("Android")
+fun SuperHeroesPreview() {
+    SuperheroesTheme {
+        SuperheroesApp()
     }
 }
--- app/src/main/java/com/example/superhero/ui/theme/Theme.kt
+++ app/src/main/java/com/example/superhero/ui/theme/Theme.kt
@@ -84,8 +84,6 @@ private val DarkColors = darkColorScheme(
 @Composable
 fun SuperheroesTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    // Dynamic color in this app is turned off for learning purposes
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
